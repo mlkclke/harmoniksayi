205e9c5b8bd50859e8c4f1b7afad5206d4cba0d3
melike çelik <mlkclke1@gmail.com>
2023-07-09 16:22:37 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+
+       System.out.print("N sayısını girin:");
+       int n = input.nextInt();
+
+       double result = 0.0;
+       for (int i=1 ; i <=n; i++){
+           result +=(1.0 / i);
+       }
+
+       System.out.println(result);
+    }
+}
